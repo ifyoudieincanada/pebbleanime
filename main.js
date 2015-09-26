92037366ae0e03a0982824b11549202451e923fc
Riley Trautman <asonix.dev@gmail.com>
2015-09-26 15:27:42 -0500

    started work on main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+var ajax = require('ajax'); // ajax object
+var UI   = require('ui'); // UI object
+
+var splashCard = new UI.Card({ // creates card to display fullscreen
+  title: "Please wait",
+  body: "downloading..."
+});
+
+splashCard.show(); // displays card
+
+var URL = 'asonix.dog'; // this is where the API url should be placed
+
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(json) {
+    // json variable is data returned from server
+    console.log(json);
+  },
+  function(error) {
+    // error returned from server or lack thereof
+    console.log('Ajax failed: ' + error);
+  }
+);
+
+function showList(jsArray) {
+  for (var i = 0; i < jsArray.length; i++) {
+    console.log(jsArray[i]);
+  }
+}
