06a4c38c7e434faac932d1d3f5d8123df005cb62
Riley Trautman <asonix.dev@gmail.com>
2015-09-26 16:45:01 -0500

    update .gitignore

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/opt/pebble/Pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'basalt', u'aplite']
