109c2f5f01be7425e41deb96724b7b61201ee572
Riley Trautman <asonix.dev@gmail.com>
2015-10-04 13:38:29 -0500

    Fix URL requires

--- src/js/url.js
+++ src/js/url.js
@@ -1,14 +1,17 @@
 /* ------ API URLs ------ */
 
-function getAnimeUrl(username) {
-  return 'http://myanimelist.net/malappinfo.php?u=' + username + '&status=all&type=anime';
-}
+var url = {
+  getAnimeUrl : function(username) {
+    return 'http://myanimelist.net/malappinfo.php?u=' + username + '&status=all&type=anime';
+  },
 
-function updateAnimeUrl(id) {
-  return  'http://myanimelist.net/api/animelist/update/' + id + '.xml';
-}
+  updateAnimeUrl : function(id) {
+    return  'http://myanimelist.net/api/animelist/update/' + id + '.xml';
+  },
 
-function deleteAnimeUrl(id) {
-  return 'http://myanimelist.net/api/animelist/delete/' + id + '.xml';
-}
+  deleteAnimeUrl : function(id) {
+    return 'http://myanimelist.net/api/animelist/delete/' + id + '.xml';
+  }
+};
 
+this.export = url;
