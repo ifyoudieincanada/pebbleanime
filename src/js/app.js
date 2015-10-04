854cffc5ae280f2180d56cec467e985ed4babf22
Riley Trautman <asonix.dev@gmail.com>
2015-10-03 23:29:26 -0500

    Moved nested functions out of main

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,45 @@ var animeLists = {
   dropped:   new AnimeList.AnimeList('dropped')
 };
 
+/* ------ ANIME LIST INTERACTIONS ------ */
+
+function setAnime(list) {
+  var i;
+  var watching = [];
+  var hold = [];
+  var completed = [];
+  var plan = [];
+  var dropped = [];
+
+  for (i = 0; i < list.length; i++) {
+    if (list[i].series_animedb_id === '1') {
+      watching.push(list[i]);
+    } else if (list[i].series_animedb_id === '3') {
+      hold.push(list[i]);
+    } else if (list[i].series_animedb_id === '2') {
+      completed.push(list[i]);
+    } else if (list[i].series_animedb_id === '6') {
+      plan.push(list[i]);
+    } else if (list[i].series_animedb_id === '4') {
+      dropped.push(list[i]);
+    }
+  }
+
+  animeLists.watching.setAnimeList(watching);
+  animeLists.hold.setAnimeList(hold);
+  animeLists.completed.setAnimeList(completed);
+  animeLists.plan.setAnimeList(plan);
+  animeLists.dropped.setAnimeList(dropped);
+}
+
+function getStoredAnime() {
+  animeLists.watching.fetchData();
+  animeLists.hold.fetchData();
+  animeLists.completed.fetchData();
+  animeLists.plan.fetchData();
+  animeLists.dropped.fetchData();
+}
+
 /* ------ GLOBAL ARRAYS ------ */
 
 var OPTIONS =
@@ -260,42 +299,6 @@ function statusOptions(index){
 /* ------ MAIN FUNCTION ------ */
 
 function main() {
-  function setAnime(list) {
-    var i;
-    var watching = [];
-    var hold = [];
-    var completed = [];
-    var plan = [];
-    var dropped = [];
-
-    for (i = 0; i < list.length; i++) {
-      if (list[i].series_animedb_id === '1') {
-        watching.push(list[i]);
-      } else if (list[i].series_animedb_id === '3') {
-        hold.push(list[i]);
-      } else if (list[i].series_animedb_id === '2') {
-        completed.push(list[i]);
-      } else if (list[i].series_animedb_id === '6') {
-        plan.push(list[i]);
-      } else if (list[i].series_animedb_id === '4') {
-        dropped.push(list[i]);
-      }
-    }
-
-    animeLists.watching.setAnimeList(watching);
-    animeLists.hold.setAnimeList(hold);
-    animeLists.completed.setAnimeList(completed);
-    animeLists.plan.setAnimeList(plan);
-    animeLists.dropped.setAnimeList(dropped);
-  }
-
-  function getStoredAnime() {
-    animeLists.watching.fetchData();
-    animeLists.hold.fetchData();
-    animeLists.completed.fetchData();
-    animeLists.plan.fetchData();
-    animeLists.dropped.fetchData();
-  }
 
   var user = Settings.option('login');
   var url = URL.getAnimeUrl(user);
